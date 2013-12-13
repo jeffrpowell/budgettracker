07a37ed6f01f193be59ce792416ebe3c0a0c2d77
csweb <webmaster@cs.byu.edu>
2013-12-13 14:34:09 -0700

    figuring out model

--- budget/models.py
+++ budget/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Account(models.Model):
 	name = models.CharField(max_length=200)
 	balance = models.DecimalField(max_digits=8, decimal_places=2)
+	asset_account = models.BooleanField()
 
 class ExpenseCategory(models.Model):
 	name = models.CharField(max_length=30)
@@ -10,6 +11,7 @@ class ExpenseCategory(models.Model):
 class Transaction(models.Model):
 	date = models.DateTimeField()
 	category = models.ForeignKey(ExpenseCategory)
-	account = models.ForeignKey(Account)
+	debit_account = models.ForeignKey(Account)
+	credit_account = models.ForeignKey(Account)
 	amount = models.DecimalField(max_digits=7, decimal_places=2)
 	memo = models.CharField(max_length=300)
