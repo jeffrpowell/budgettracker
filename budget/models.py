760fb5414e2c5f308c057c3bbb0aefd323a15353
csweb <webmaster@cs.byu.edu>
2013-12-13 14:22:40 -0700

    figuring out model

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+	name = models.CharField(max_length=200)
+	balance = models.DecimalField(max_digits=8, decimal_places=2)
+
+class ExpenseCategory(models.Model):
+	name = models.CharField(max_length=30)
+
+class Transaction(models.Model):
+	date = models.DateTimeField()
+	category = models.ForeignKey(ExpenseCategory)
+	account = models.ForeignKey(Account)
+	amount = models.DecimalField(max_digits=7, decimal_places=2)
+	memo = models.CharField(max_length=300)
