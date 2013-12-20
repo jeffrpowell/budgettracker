df16b7b69d3a301887eee2ec0fc787ae8bddd674
csweb <webmaster@cs.byu.edu>
2013-12-20 14:42:53 -0700

    add transaction, and starting projected and difference values on index

--- budget/models.py
+++ budget/models.py
@@ -1,23 +1,29 @@
 from django.db import models
+from django.forms import ModelForm
 
 class AccountCategory(models.Model):
 	name = models.CharField(max_length=30)
 	def __unicode__(self):
-                return self.name
+		return self.name
 	
 class Account(models.Model):
 	name = models.CharField(max_length=200)
 	balance = models.DecimalField(max_digits=8, decimal_places=2)
 	category = models.ForeignKey(AccountCategory)
 	def __unicode__(self):
-                return self.name
+		return self.name
 
 class Transaction(models.Model):
-	date = models.DateField()
+	date = models.DateField(auto_now_add=True)
 	to_account = models.ForeignKey(Account, related_name="to_account")
 	from_account = models.ForeignKey(Account, related_name="from_account")
 	amount = models.DecimalField(max_digits=7, decimal_places=2)
 	prediction = models.BooleanField()
 	memo = models.CharField(max_length=300, default=None, blank=True, null=True)
 	def __unicode__(self):
-                return unicode(self.amount)
+		return unicode(self.amount)
+                
+class TransactionForm(ModelForm):
+    class Meta:
+    	model = Transaction
+    	fields = ['amount', 'memo']
--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 
-from budget.models import Transaction, Account, AccountCategory
+from budget.models import Transaction, Account, AccountCategory, TransactionForm
 
 def index(request):
     categories = AccountCategory.objects.all().exclude(name='Bank Accounts')
@@ -11,7 +11,12 @@ def index(request):
     data = []
     for cat in categories:
     	entry = {'cat': cat}
-    	entry['accounts'] = Account.objects.filter(category=cat)
+    	accounts = Account.objects.filter(category=cat)
+    	all_accounts = []
+    	for acct in accounts:
+    		acct_entry = {'acct': acct, 'pred': 2, 'diff': 3}
+    		all_accounts.append(acct_entry)
+    	entry['accounts'] = all_accounts
     	data.append(entry)
     context['data'] = data
     return render(request, 'budget/index.html', context)
@@ -37,7 +42,23 @@ def category(request, cid):
 	})
 
 def addtransaction(request, to_account=None):
-	context = {}
-	if to_account:
-		context['to_account'] = Account.objects.get(pk=to_account)
-	return render(request, 'budget/addtransaction.html', context)
+    context = {}
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            f = TransactionForm(request.POST)
+            trans = f.save(commit = False)
+            trans.to_account = Account.objects.get(pk=request.POST['to_account'])
+            trans.from_account = Account.objects.get(name="Checking Account")
+            trans.prediction = False
+            trans.save()
+            trans.to_account.balance = trans.to_account.balance + trans.amount
+            trans.to_account.save()
+            trans.from_account.balance = trans.from_account.balance - trans.amount
+            trans.from_account.save()
+            return HttpResponseRedirect('/budget/')
+    else:
+        context['form'] = TransactionForm()
+    if to_account:
+    	context['to_account'] = Account.objects.get(pk=to_account)
+   	return render(request, 'budget/addtransaction.html', context)
